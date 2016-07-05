838f150627aa9feb184e134b5825b13f1364a6fa
Emile <emile.app@gmail.com>
2016-07-05 09:26:34 +0200

    added statis todo search

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,11 +1,14 @@
 var React = require('react');
 var TodoList = require('TodoList');
 var TodoForm = require('TodoForm');
+var TodoSearch = require("TodoSearch");
 
 var TodoApp = React.createClass({
   getInitialState: function(){
     return (
       {
+        showCompleted: false,
+        searchText: '',
         todos : [
           {
             id: 1,
@@ -30,11 +33,18 @@ var TodoApp = React.createClass({
   handleAddTodo: function(text){
     alert("new todo: " + text);
   },
+  handleSearch: function(showCompleted, searchText){
+    this.setState({
+      showCompleted: showCompleted,
+      searchText: searchText.toLowerCase()
+    });
+  },
   render: function(){
     var {todos} = this.state;
     console.log("todos: ", todos);
     return (
       <div>
+        <TodoSearch onSearch={this.handleSearch}/>
         <TodoList todos={todos}/>
         <TodoForm onAddTodo={this.handleAddTodo}/>
       </div>
