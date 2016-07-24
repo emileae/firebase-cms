2a1283f10c8b857b1f647ebf4184022593d25024
emileae <Omoikane@Emiles-Mac-mini.bkl38.local>
2016-07-24 15:39:29 +0200

    update todos on firebase, integrated async tests

--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -2,6 +2,7 @@ import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 var expect = require('expect');
 
+import firebase, {firebaseRef} from 'app/firebase';
 var actions = require('actions');
 
 var createMockStore = configureMockStore([thunk]);
@@ -76,13 +77,50 @@ describe("Actions", () => {
     expect(res).toEqual(action);
   });
 
-  it('should generate toggle todo action', () => {
+  it('should generate update todo action', () => {
     var action = {
-      type: "TOGGLE_TODO",
-      id: 1
+      type: "UPDATE_TODO",
+      id: 1,
+      updates: {completed: false}
     };
-    var res = actions.toggleTodo(action.id);
+    var res = actions.updateTodo(action.id, action.updates);
     expect(res).toEqual(action);
   });
 
+  describe('Tests with firebase todos', () => {
+    var testTodoRef;
+    //beforeEach, afterEach  this is a mocha function code to run before each test
+    beforeEach((done) => {
+      testTodoRef = firebaseRef.child('todos').push();
+
+      testTodoRef.set({
+        text: 'Something to do',
+        completed: false,
+        createdAt: 123456
+      }).then(() => done());// only 1 line ES6 arrow fn .then
+    });
+    afterEach((done) => {
+      testTodoRef.remove().then(() => done());// clean up dummy data afterwards
+    });
+
+    it('should toggle todo and dispatch UPDATE_TODO action', (done) => {
+      const store = createMockStore({});
+      const action = actions.startToggleTodo(testTodoRef.key, true);
+
+      store.dispatch(action).then(() => {
+        const mockActions = store.getActions();
+
+        expect(mockActions[0]).toInclude({
+          type: 'UPDATE_TODO',
+          id: testTodoRef.key,
+        });
+        expect(mockActions[0].updates).toInclude({
+          completed: true
+        });
+        expect(mockActions[0].updates.completedAt).toExist();
+
+        done();
+      }, done);
+    });
+  });
 });
--- app/tests/components/Todo.test.jsx
+++ app/tests/components/Todo.test.jsx
@@ -4,7 +4,9 @@ var expect = require('expect');
 var $ = require('jQuery');
 var TestUtils = require('react-addons-test-utils');
 
-var {Todo} = require('Todo');
+import * as actions from 'actions';
+import {Todo} from 'Todo';
+//var {Todo} = require('Todo');
 
 describe ('Todo', () => {
 
@@ -18,18 +20,15 @@ describe ('Todo', () => {
       text: "write todo test",
       completed: true
     };
+    var action = actions.startToggleTodo(todoData.id, !todoData.completed);
 
     var spy = expect.createSpy();
     var todo = TestUtils.renderIntoDocument(<Todo {...todoData} dispatch={spy}/>);
-
     var $el = $(ReactDOM.findDOMNode(todo));
+
     TestUtils.Simulate.click($el[0]);
 
-    expect(spy).toHaveBeenCalledWith({
-      type: "TOGGLE_TODO",
-      id: todoData.id
-    });
-
+    expect(spy).toHaveBeenCalledWith(action);
 
   });
 
